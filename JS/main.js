e413e5d4edace0c5dd323fd23c9ee1c383be9ad8
M-Vaidehi-R <m_ramesh@fanshaweonline.ca>
2022-11-17 18:12:53 -0500

    when clicking on thumbnails, car details showing

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+import miniThumbnails from './components/thumbnails.js';
+import miniLightBox from './components/lightbox.js';
+
+(() => {
+
+    console.log("hey"); 
+    const { createApp } = Vue  //lifecycle of vue => create, mount, update, destroyed
+
+   
+    createApp({  //vue object
+
+        created() {   
+              //fetching datab from data.json    
+              fetch('./data.json')  //go and get the remote data
+              .then(res => res.json()) //convert the json object to plain JS object
+              .then(data => this.miniData = data)  //store the data in the vue instance
+              .catch(error => console.error(error)); 
+        },
+
+        data(){
+            return{
+                miniData: {},
+                lightboxData: {},
+                showLightBox: false
+            }
+        },
+
+        methods: {
+            loadLightBox(item) {  //when u click on thumbnail, this function invokes
+                this.lightboxData = item;
+                this.showLightBox=true;
+            }
+        },
+
+        components: {
+            minithumb: miniThumbnails,
+            minilb: miniLightBox
+        }
+
+    }).mount('#app')
+
+})()
