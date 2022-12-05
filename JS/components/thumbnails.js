928939bbd7fae5af2dcdf01e70f37d23c704d6b7
Mamatha Vaidehi Ramesh <m_ramesh150220@fanshaweonline.ca>
2022-12-05 11:43:43 -0500

    updated HTML structure

--- JS/components/thumbnails.js
+++ JS/components/thumbnails.js
@@ -9,6 +9,8 @@ export default{
     `
     <li @click="loadlightboxdata">
     <img :src='"Images/" + mini.carimg' alt="Mini Cooper Image">
+    <h2> {{ mini.name }} </h2>
+    <h3> {{ mini.price }} </h3>
     </li>
     `,
 
